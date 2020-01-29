80f78140778508b59f88f2561a473dd1a3d1c3f5
Cody Frazier <codyfrazier@me.com>
2020-01-28 21:37:39 -0800

    pages and users showing, need to change users based on page

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Nav({ params, setParams}){
 	
 	return (
-		<nav>
+		<nav id = 'webNav'>
 			<a href = '#home'>Home</a>
 			<a href = '#users'>Users</a>
 		</nav>
--- src/Users.js
+++ src/Users.js
@@ -7,26 +7,45 @@ function Users(){
 
 	const [users, setUsers] = useState([]);
 	const [count, setCount] = useState(0);
+	const [pages, setPages] = useState([]);
+	
 	const fetchUser = async() => {
-		const userAPI = `${API}${ count ? `/${count}` : ''}`;
-		console.log(userAPI);
+		const userAPI = `${API}` + `${ count ? '/${ count }' : ''}`;
 		return axios.get(userAPI).then(res => res.data)
 		}
 
 	useEffect(()=>{
-
+		const nums = [];
+		for(let i = 0; i < 24; i++){
+			nums.push(i);
+		}
 		fetchUser().then( res => { 
 			console.log(res.users);
-			setUsers(res.users) 
+			setUsers(res.users);
+			setPages(nums);
 			})
 
 		
 		
 	},[])
-
-	return (<ul>
-		hiiiii
-		</ul>)
+	return (
+		<div>
+			<nav id = 'pageNav'>{
+				pages.map((page, idx) => {
+					return(
+						<a className = 'page' href = { `${ API }${ page ? '/page' : '' }` }>{ idx }</a>
+					)
+				})
+			}</nav>
+			<ul>{
+				users.map((user,idx) => {
+					return (
+						<li key = { idx }>{ user.fullName }</li>
+					)
+				})
+			}</ul>
+		</div>
+	)
 }
 
 export default Users;
